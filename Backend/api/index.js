c91e682f617a26a0b6d803c8446828b804e052e3
PramesRay <prames21001@mail.unpad.ac.id>
2024-06-18 22:56:59 +0700

    Commit specify the view engine

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -39,7 +39,8 @@ database.connect((err) => {
   console.log("Database Connected")
 })
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('payment')
