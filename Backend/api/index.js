59107258e1ae8c2b86c1077b3cb08912eb5c2e91
PramesRay <prames21001@mail.unpad.ac.id>
2024-06-19 08:47:16 +0700

    Commit configure session tobe able to cross domain

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -14,6 +14,7 @@ const moment = require('moment')
 
 const corsConfig = {
   origin: "*",
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credential: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
 }
